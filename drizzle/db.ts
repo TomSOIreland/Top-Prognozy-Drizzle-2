f6d7977cc27277f16f2b4697ef6cde77003edfba
Tom Malkiewicz <tom.malkiewicz@specialolympics.ie>
2025-03-21 23:37:13 +0000

    fixing db

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,9 +1,11 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
+
 export const db = drizzle({
   schema,
   connection: {
+    prepare: false,
     url: process.env.DATABASE_URL,
   },
 });
